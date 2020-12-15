1e4a7a46faf4a71ad1b0fc4692441a6564a7b213
Seyed Basim <Seyedbasim95@outlook.com>
2020-12-15 09:16:08 +0530

    New Script added Start Stop VM

---
+++ Start and Stop VM.ps1
@@ -0,0 +1,10 @@
+﻿Connect-AzAccount -SubscriptionId "ba5a0a9c-3264-4cd1-8576-ed7a3fd8b8ad"
+
+$vm = Get-AzVM -ResourceGroupName "AD" -Name "hp01-1" -Status
+$Status = $vm.Statuses[1].Code.Split("/")[1]
+
+if($Status -eq "deallocated"){
+    Start-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
+}else{
+    Stop-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
+}
